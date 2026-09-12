78729bb21b5f0a29b2642b1c34e46e0e71940d69
NextRTOS <pcwin1869@gmail.com>
2026-09-12 19:47:31 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,39 +6,66 @@ use limine::request::FramebufferRequest;
 
 #[path = "../drivers/net/realtek/rtl8153.rs"]
 mod rtl8153;
+mod pci;
 
 #[used]
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let mut net_card = rtl8153::Realtek8153::new();
-    net_card.init();
-    let _mac = net_card.get_mac();
+pub struct TitaniumKernel {
+    pub version: &'static str,
+    pub net_driver: rtl8153::Realtek8153,
+}
 
-    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.response() {
-        if let Some(framebuffer) = framebuffer_response.framebuffers().first() {
-            let fb_ptr = framebuffer.address() as *mut u32;
-            let width = framebuffer.width as usize;
-            let height = framebuffer.height as usize;
-            let pitch = framebuffer.pitch as usize / 4; 
+impl TitaniumKernel {
+    pub fn new() -> Self {
+        Self {
+            version: "0.0.4",
+            net_driver: rtl8153::Realtek8153::new(),
+        }
+    }
 
-            let titanium_bg: u32 = 0x00_1A_1C_23;
-            let success_green: u32 = 0x00_00_FF_00;
+    pub fn boot(&mut self) {
+        self.net_driver.init();
+        
+        let has_gpu = unsafe { pci::PciScanner::find_device_type(0x03) };
+        let has_net = unsafe { pci::PciScanner::find_device_type(0x02) };
 
-            for y in 0..height {
-                for x in 0..width {
-                    unsafe {
-                        if x < 50 && y < 50 {
-                            fb_ptr.add(y * pitch + x).write_volatile(success_green);
-                        } else {
-                            fb_ptr.add(y * pitch + x).write_volatile(titanium_bg);
+        if has_gpu {
+            self.draw_screen(has_net);
+        }
+    }
+
+    fn draw_screen(&self, net_success: bool) {
+        if let Some(fb_res) = FRAMEBUFFER_REQUEST.response() {
+            if let Some(fb) = fb_res.framebuffers().first() {
+                let fb_ptr = fb.address() as *mut u32;
+                let width = fb.width as usize;
+                let height = fb.height as usize;
+                let pitch = fb.pitch as usize / 4;
+
+                let bg_color = 0x00_1A_1C_23;
+                let net_color = if net_success { 0x00_00_FF_00 } else { 0x00_FF_00_00 };
+
+                for y in 0..height {
+                    for x in 0..width {
+                        unsafe {
+                            if x < 40 && y < 40 {
+                                fb_ptr.add(y * pitch + x).write_volatile(net_color);
+                            } else {
+                                fb_ptr.add(y * pitch + x).write_volatile(bg_color);
+                            }
                         }
                     }
                 }
             }
         }
     }
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let mut kernel = TitaniumKernel::new();
+    kernel.boot();
 
     x86_64::instructions::interrupts::disable();
     loop {
