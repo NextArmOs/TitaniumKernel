f71279d738a8dcb9eb68a66ae0a9a1c7ef11b9c2
CopperUI <TitaniumKernel@gmail.com>
2026-09-12 22:57:32 +0300

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,45 +4,45 @@
 use core::panic::PanicInfo;
 use limine::request::FramebufferRequest;
 
-#[path = "../drivers/net/realtek/rtl8153.rs"]
-mod rtl8153;
-#[path = "../drivers/sound/speaker.rs"]
-mod speaker;
-mod pci;
+mod ui;
 
 #[used]
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
-pub struct TitaniumKernel {
-    pub version: &'static str,
-}
-
-impl TitaniumKernel {
-    pub fn new() -> Self {
-        Self { version: "0.0.5" }
-    }
-
-    pub fn boot(&mut self) {
-        unsafe {
-            speaker::PcSpeaker::play_tone(1000);
-        }
-    }
-}
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let mut kernel = TitaniumKernel::new();
-    kernel.boot();
+    if let Some(fb_res) = FRAMEBUFFER_REQUEST.response() {
+        if let Some(fb) = fb_res.framebuffers().first() {
+            let fb_ptr = fb.address() as *mut u32;
+            let width = fb.width as usize;
+            let height = fb.height as usize;
+            let pitch = fb.pitch as usize / 4;
+            for i in 0..(height * pitch) {
+                unsafe { fb_ptr.add(i).write_volatile(0x00_1A_1C_23); }
+            }
 
-    x86_64::instructions::interrupts::disable();
+            unsafe {
+                ui::CopperUI::draw_rect(fb_ptr, pitch, 20, 20, 200, 40, 0x00_B8_73_33);
+
+                ui::CopperUI::draw_rect(fb_ptr, pitch, 22, 22, 196, 36, 0x00_11_13_17);
+
+                let mut char_x = 30;
+                let char_y = 37;
+
+                for _ in 0..14 {
+                    ui::CopperUI::draw_block_char(fb_ptr, pitch, char_x, char_y, 0x00_FF_D7_00);
+                    char_x += 12;
+                }
+            }
+        }
+    }
+
     loop {
-        x86_64::instructions::hlt();
+        unsafe { core::arch::asm!("hlt"); }
     }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {
-        x86_64::instructions::hlt();
-    }
+    loop {}
 }
