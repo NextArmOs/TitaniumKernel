af88b501d70e0d0b9873eb154a96fcf3a3afd810
NextRTOS <pcwin1869@gmail.com>
2026-09-12 14:34:59 +0300

    rtl8153.rs

---
+++ drivers/net/realtek/rtl8153.rs
@@ -0,0 +1,17 @@
+pub struct Realtek8153 {
+    pub mac_address: [u8; 6],
+    pub is_connected: bool,
+}
+
+impl Realtek8153 {
+    pub fn new() -> Self {
+        Self {
+            mac_address: [0x00, 0xE0, 0x4C, 0x68, 0x01, 0x11],
+            is_connected: false,
+        }
+    }
+
+    pub fn init(&mut self) {
+        self.is_connected = true;
+    }
+}
